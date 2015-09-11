6ecf4638d1240528f9e3472fc32620329967b0c8
MrLys <thomaslyseggen@gmail.com>
2015-09-11 12:23:43 +0200

    fixed temperaturconverter

--- week2/temperatureconverter.py
+++ week2/temperatureconverter.py
@@ -3,20 +3,10 @@ import math
 
 def converter():
     print "Fahrenheit to celsius convertion:"
-    try:
-        temp_in_F = argv[1]
-        temp_in_C = str(math.ceil(((int(float(temp_in_F)))-32)*(5./9)))
-        print (temp_in_F + " Fahrenheit = " + temp_in_C + " Celsius")
-        
-    except IndexError:
-        print "IndexError: No argument given."
-        print argv[0]+" <INPUT>"
-        print "Example: " + argv[0] + "75.2"
-    except ValueError:
-        print "ValueError: "
-        if ',' in argv[1]:
- 
-            print "Use " + argv[1].split(',')[0] + "."+argv[1].split(',')[1] + " instead of " + argv[1]
+    temp_in_F = float(argv[1])
+    print "%.1f" %temp_in_F
+    temp_in_C = ((float(temp_in_F))-32)*(5./9)
+    print "{:.1f} Fahrenheit is equal to {:.1f} Celsius".format(temp_in_F,temp_in_C)
 
 
 converter()
